651184e7785579a11c2cdefe1f2d478953768283
Bassem Alnaamat <bassem.alnaamat@gmail.com>
2022-10-23 20:34:45 +0200

    '..A..'

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let hours = document.querySelector('.h');
 let minutes = document.querySelector('.m');
 let seconds = document.querySelector('.s');
 
+let hour = document.querySelector('.hours');
+let minute = document.querySelector('.minutes');
+let second = document.querySelector('.seconds');
+
 
 setInterval(() => {
     let time = new Date();
@@ -9,14 +13,28 @@ setInterval(() => {
     let s = time.getSeconds();
     let sDegree = (((s / 60) * 360) + 360)
     seconds.style.transform = `rotate(${sDegree}deg)`;
+    second.innerHTML = checkZero(s);
 
     let m = time.getMinutes();
     let mDegree = (((m / 60) * 360) + 360);
     minutes.style.transform = `rotate(${mDegree}deg)`;
+    minute.innerHTML = checkZero(m);
 
     let h = time.getHours();
     let hDegree = (((h / 12) * 360) + 360);
     hours.style.transform = `rotate(${hDegree}deg)`;
+    hour.innerHTML = checkZero(h);
     
 }, 1000);
 
+
+
+function checkZero(i) {
+    if (i < 10) {
+        i = `0${i}`;
+       
+    }
+    return i;
+}
+
+checkZero();
