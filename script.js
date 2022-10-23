b78d837b2a492cff1b214e7bf02590bc554ce317
Bassem Alnaamat <bassem.alnaamat@gmail.com>
2022-10-23 06:53:03 +0200

    '..dd..'

--- script.js
+++ script.js
@@ -5,32 +5,18 @@ let seconds = document.querySelector('.s');
 
 setInterval(() => {
     let time = new Date();
-    let h = time.getHours();
-    let m = time.getMinutes();
+
     let s = time.getSeconds();
-    let y = time.getFullYear().toString();
-    let mo = time.getMonth() + 1;
-    let d = time.getDate();
-    h = checkZero(h);
-    m = checkZero(m);
-    s = checkZero(s);
-    mo = checkZero(mo);
-    d = checkZero(d);
+    let sDegree = (((s / 60) * 360) + 360)
+    seconds.style.transform = `rotate(${sDegree}deg)`;
 
-    document.querySelector('.hours').innerHTML = h;
-    document.querySelector('.minutes').innerHTML = m;
-    document.querySelector('.seconds').innerHTML = s;
-    document.querySelector('.year').innerHTML = y.slice(2);
-    document.querySelector('.month').innerHTML = mo;
-    document.querySelector('.day').innerHTML = d;
+    let m = time.getMinutes();
+    let mDegree = (((m / 60) * 360) + 360);
+    minutes.style.transform = `rotate(${mDegree}deg)`;
 
-    seconds.style.transform = 'rotate(' + (((s / 60) * 360) + 360) + 'deg)';
-    minutes.style.transform = 'rotate(' + ((m / 60 ) * 360) + 'deg)';
-    hours.style.transform = 'rotate(' + ((h / 12) * 360) + 'deg)';
+    let h = time.getHours();
+    let hDegree = (((h / 12) * 360) + 360);
+    hours.style.transform = `rotate(${hDegree}deg)`;
     
 }, 1000);
 
-function checkZero(i) {
-    if(i < 10) { i = '0' + i}
-    return i;
-}
